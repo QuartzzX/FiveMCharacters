0d57fec492641d6eafe80d359faa91723f4b4b16
QuartzzX <oliverlongnz@gmail.com>
2022-04-01 17:11:54 +1300

    first commit

---
+++ crystal.js
@@ -0,0 +1,64 @@
+// noinspection JSUnusedGlobalSymbols
+
+/**
+ Get the formatted priors of a character from JSON.
+ @param character - The path to the JSON character to get priors of.
+ */
+function getPriors(character) {
+    return (character.priors.length === 0) ? "No Priors." : "Priors for: " + character.priors.join(", ") + "."
+}
+
+/**
+ Get the formatted warrants of a character from JSON.
+ @param character - The path to the JSON character to get warrants of.
+ */
+function getWarrants(character) {
+    return (character.warrants.length === 0) ? "No Active Warrants." : "Active Warrant(s): " + character.warrants.join(", ") + "."
+}
+
+/**
+ Get the formatted boolean of whether a character is known to police from JSON.
+ @param character - The path to the JSON character to get the known status of.
+ */
+function getIsKnownToPolice(character) {
+    return (character.knownToPolice) ? "Known To Police" : "Not Known to Police" + "."
+}
+
+
+/**
+ * Gets the notes of a character from JSON.
+ * @param character - The path to the JSON character to get the note of.
+ */
+function getNotes(character) {
+    return (character.notes) + "."
+}
+
+function getCharacterByName(name) {
+    for (const character of Website.Info.Crystal) {
+        if (character.name === name) {
+            return character
+        }
+    }
+}
+
+Website.Info.Crystal.forEach((character) => {
+    document.getElementById("select").innerHTML += "<option>" + character.name + "</option>"
+})
+
+
+function update() {
+    let select = document.getElementById("select")
+    let option = select.options[select.selectedIndex]
+    if (option.value === "Select Character") {
+        document.getElementById("civ-information").innerHTML = "No Information"
+        document.getElementById("civ-notes").innerHTML = "No Notes"
+        document.getElementById("civ-command").innerHTML = ""
+        return;
+
+    }
+    let character = getCharacterByName(option.value)
+    document.getElementById("civ-information").innerHTML = "Address: " + character.address.postal + ", " + character.address.roadName + ", " + character.address.area + ", " + character.address.city + "<br>" + "Date of Birth: " + character.birthday + "<br>" + getPriors(character) + "<br>" + getWarrants(character) + "<br>" + getIsKnownToPolice(character) + "<br>" + character.licence
+    document.getElementById("civ-notes").innerHTML = getNotes(character)
+    document.getElementById("civ-command").innerHTML = (`/do QP '${character.name}': Lives at ${character.address.postal}, ${character.address.roadName}, ${character.address.area}, ${character.address.city}. Born on ${character.birthday}. ${getPriors(character)} ${getWarrants(character)} ${getIsKnownToPolice(character)} ${character.licence} .`)
+
+}
---
+++ data.js
@@ -0,0 +1,128 @@
+Website = [];
+Website.Info = {
+    "Beehive": [
+        {
+            "name": "Jonathan Yates",
+            "address": {
+                "city": "Wellington",
+                "area": "Porirua",
+                "postal": "3001",
+                "roadName": "Panorama Drive"
+            },
+            "birthday": "1998-06-06",
+            "priors": [
+                " 4x Speeding",
+                " 3x Reckless Driving"
+
+            ],
+            "warrants": [],
+            "knownToPolice": false,
+            "licence": "Valid Class 1, 2 & 3 Drivers Licences",
+            "notes": [
+                " Works for exclusive car company (Car & Company Co.) therefore getting access to sports cars often",
+            ]
+
+        },
+        {
+            "name": "Micheal Evangelidakis",
+            "address": {
+                "city": "Wellington",
+                "area": "Te Aro",
+                "postal": "7659",
+                "roadName": "Bay City Ave"
+            },
+            "birthday": "1990-07-09",
+            "priors": [],
+            "warrants": [],
+            "knownToPolice": false,
+            "licence": "Valid Class 1, 2 & 3 Licence + PI License",
+            "notes": [
+                " Works as security for Mr John Musk and drives a white G-Wagon Plate G63",
+                " Works as security for Ms Becky and drives a matte black G-Wagon Plate Secure",
+                " Works as security for Mr Don Henley and drives a desert tan G-Wagon Plate Tech (Tech company & co inc.)",
+                " Owns the Casino @7289"
+            ]
+        },
+        {
+            "name": "Timothy Long",
+            "address": {
+                "city": "Wellington",
+                "area": "Porirua",
+                "postal": "3027",
+                "roadName": "Marina Drive"
+            },
+            "birthday": "1988-05-31",
+            "priors": [
+                " 3x Reckless driving"
+            ],
+            "warrants": [],
+            "knownToPolice": false,
+            "licence": "Valid Class 1, 2, 3, 4 & 5 Licences",
+            "notes": [
+                " Jumps between jobs trying to make a living"
+                ]
+        },
+        {
+            "name": "Jeff Jones",
+            "address": {
+                "city": "Wellington",
+                "area": "Johnsonville",
+                "postal": "6031",
+                "roadName": ""
+            },
+            "birthday": "1991-06-18",
+            "priors": [
+            ],
+            "warrants": [],
+            "knownToPolice": false,
+            "licence": "Valid Class 1, 2 & 3 Licence",
+            "notes": [
+                " Owns a dog named Pablo"
+            ]
+        },
+        {
+            "name": "Jimmy Martin",
+            "address": {
+                "city": "Wellington",
+                "area": "Paekakariki",
+                "postal": "2027",
+                "roadName": "O'Neil Way"
+            },
+            "birthday": "1989-03-31",
+            "priors": [
+            ],
+            "warrants": [],
+            "knownToPolice": false,
+            "licence": "Valid Class 1, 2 & 3 Licence",
+            "notes": [
+                " Owns Auto Repairs @ Porirua"
+            ]
+        }
+    ],
+
+    "Crystal": [
+        {
+            "name": "Terry Jones",
+            "address": {
+                "city": "Los Santos",
+                "area": "Blaine County",
+                "postal": "3001",
+                "roadName": "Panorama Drive"
+            },
+            "birthday": "1983-06-28",
+            "priors": [
+                " Violent protesting",
+                " Reckless Driving"
+
+            ],
+            "warrants": [],
+            "knownToPolice": true,
+            "licence": "Valid Class 1, 2 & 3 Drivers Licences + Weapon Licence",
+            "notes": [
+                " Patriotic American guy",
+            ]
+        },
+
+    ]
+
+}
